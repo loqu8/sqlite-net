e26a6a8a9f584fa093dc5456eeb68abfc3ecdd59
Tim Uy <tim@loqu8.com>
2016-12-17 08:19:57 -0800

    add multiquery test

---
+++ tests/SQLite.Tests.Shared/MultiQueryTest.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using System.Text;
+using SQLite;
+
+#if NETFX_CORE
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using SetUp = Microsoft.VisualStudio.TestPlatform.UnitTestFramework.TestInitializeAttribute;
+using TestFixture = Microsoft.VisualStudio.TestPlatform.UnitTestFramework.TestClassAttribute;
+using Test = Microsoft.VisualStudio.TestPlatform.UnitTestFramework.TestMethodAttribute;
+#else
+using NUnit.Framework;
+#endif
+
+using System.IO;
+
+namespace SQLite.Tests
+{
+    [TestFixture]
+    public class MultiQueryTest
+    {
+        TestDb _db;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _db = new TestDb();
+            _db.CreateTable<Product>();
+
+            var p1 = new Product { Name = "One", };
+            var p2 = new Product { Name = "Two", };
+            var p3 = new Product { Name = "Three", };
+            _db.InsertAll(new[] { p1, p2, p3 });
+        }
+
+        [Test]
+        public void InsertNCount()
+        {
+            var result = _db.ExecuteScalar<int>("INSERT INTO Product (Name) VALUES ('Four');SELECT COUNT(1) FROM Product");
+            Assert.AreEqual(result, 4);
+        }
+
+        [Test]
+        public void InsertNList()
+        {
+            var cmd = _db.CreateCommand("INSERT INTO Product (Name) VALUES ('Five');SELECT * FROM Product");
+            var result = cmd.ExecuteQuery<Product>();
+            Assert.AreEqual(result.Count, 4);
+        }
+    }
+}
